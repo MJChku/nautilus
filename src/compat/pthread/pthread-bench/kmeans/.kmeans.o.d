40e242e91d4e19da8405b6d5d4a2ac4438dd99a8
MJChku <mjc97cooper@gmail.com>
2020-10-15 03:08:35 -0500

    add leftout pthread benchmark

---
+++ src/compat/pthread/pthread-bench/kmeans/.kmeans.o.d
@@ -0,0 +1,32 @@
+src/compat/pthread/pthreads-test/kmeans/kmeans.o: \
+  src/compat/pthread/pthreads-test/kmeans/kmeans.c include/autoconf.h \
+  src/compat/pthread/pthreads-test/kmeans/../include/stddefines.h \
+  include/nautilus/nautilus.h include/nautilus/percpu.h \
+  /usr/lib/llvm-10/lib/clang/10.0.0/include/stddef.h \
+  /usr/lib/llvm-10/lib/clang/10.0.0/include/__stddef_max_align_t.h \
+  include/nautilus/msr.h include/nautilus/naut_types.h \
+  include/nautilus/smp.h include/dev/apic.h include/nautilus/spinlock.h \
+  include/nautilus/intrinsics.h include/nautilus/atomic.h \
+  include/nautilus/cpu.h include/nautilus/cpu_state.h \
+  include/nautilus/instrument.h include/nautilus/mm.h \
+  include/nautilus/list.h include/nautilus/naut_string.h \
+  include/nautilus/buddy.h include/nautilus/queue.h \
+  include/nautilus/printk.h \
+  /usr/lib/llvm-10/lib/clang/10.0.0/include/stdarg.h \
+  include/dev/serial.h include/nautilus/thread.h \
+  include/nautilus/cachepart.h include/nautilus/aspace.h \
+  include/nautilus/idt.h include/asm/lowlevel.h include/nautilus/gdt.h \
+  include/nautilus/scheduler.h include/nautilus/vc.h include/dev/ps2.h \
+  include/dev/ioapic.h include/nautilus/paging.h \
+  include/nautilus/limits.h include/nautilus/naut_assert.h \
+  include/nautilus/barrier.h include/nautilus/numa.h \
+  include/arch/x64/main.h include/nautilus/shell.h \
+  include/nautilus/libccompat.h include/compat/linux/sysconf.h \
+  src/compat/pthread/pthreads-test/kmeans/../include/../../pthread.h \
+  src/compat/pthread/pthreads-test/kmeans/../include/../../nk/pte_types.h \
+  include/nautilus/semaphore.h include/nautilus/waitqueue.h \
+  src/compat/pthread/pthreads-test/kmeans/../include/../../sched.h \
+  include/nautilus/setjmp.h \
+  /usr/lib/llvm-10/lib/clang/10.0.0/include/limits.h \
+  /usr/include/limits.h \
+  src/compat/pthread/pthreads-test/kmeans/../include/map_reduce.h
